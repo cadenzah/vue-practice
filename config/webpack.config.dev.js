84c904ffb21f766ae2859f681ebeeb83e5da44f4
cadenzah <cadenzah93@gmail.com>
2020-11-15 15:32:20 +0900

    separated config files, added some dev settings

---
+++ config/webpack.config.dev.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const VueLoader = require('vue-loader');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
+module.exports = (env) => {
+  // file paths
+  const configPath = path.join(__dirname);
+  const buildPath = path.join(configPath, '..', 'build');
+
+  const config = {
+    entry: './src/main.js',
+    output: {
+      publicPath: '/',
+      filename: 'js/[name].js',
+      path: buildPath,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: '/node_modules',
+          use: 'babel-loader',
+        },
+        {
+          test: /\.vue$/,
+          use: 'vue-loader',
+        },
+        {
+          test: /\.css$/,
+          use: [
+            'vue-style-loader',
+            'css-loader',
+          ],
+        },
+      ],
+    },
+    devServer: {
+      contentBase: buildPath,
+      compress: true,
+      port: 3000,
+      // historyApiFallback: true,
+      
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+      }),
+      new VueLoaderPlugin(),
+    ],
+  };
+
+  return config;
+}
